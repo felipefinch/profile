6ff4491680a16f7252ba764f1f1be8f16a229729
Atticus <269244147+felipefinch@users.noreply.github.com>
2026-05-06 14:40:39 -0400

    Fix dropdown issue; maybe!

--- jscript/main.js
+++ jscript/main.js
@@ -1,9 +1,4 @@
 
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, options);
-});
-
 function grabMDFile(nameOfFile) {
     console.log("What is name of MD file? " + nameOfFile);
 
